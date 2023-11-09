88d0d554c2caf591e70973df0c71a555852d328f
Ivan Lintsov <ivan.linzov@gmail.com>
2023-11-09 21:09:07 +0300

    Изменен запуск тестов ver. 2

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,8 +1,8 @@
-from os import environ
-from django import setup
-
-environ.setdefault(
-    'DJANGO_SETTINGS_MODULE', 'tplab2.settings'
-)
-
-setup()
+# from os import environ
+# from django import setup
+#
+# environ.setdefault(
+#     'DJANGO_SETTINGS_MODULE', 'tplab2.settings'
+# )
+#
+# setup()
